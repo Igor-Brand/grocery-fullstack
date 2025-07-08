8c2586cfd73748001fcdecab94aedb1967ec8161
Punkrig <igorbrandaao@gmail.com>
2025-07-08 15:12:45 -0300

    Desenvolvimento: finalizando a navbar, e começando a desenvolver a home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add other routes here as needed */} 
+        </Routes>
+      </div>
     </div>
   )
 }
